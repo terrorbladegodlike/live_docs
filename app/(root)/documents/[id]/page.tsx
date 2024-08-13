ec6e50e73f1cc4d3e472bcd090d045bc613a470c
terrorbladegodlike <ternovetchii.2002@mail.ru>
2024-08-13 20:07:40 +0300

    Create Document Editor Page

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,16 @@
+// Import Components
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+
+const Document = () => {
+   return (
+      <div>
+         <Header>
+            <p className='text-white'>Test</p>
+         </Header>
+         <Editor />
+      </div>
+   )
+}
+
+export default Document
