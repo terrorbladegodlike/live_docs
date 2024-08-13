caca8dc08f11b27d1054059e96f1e65090c8a6aa
terrorbladegodlike <ternovetchii.2002@mail.ru>
2024-08-13 20:27:02 +0300

    Adding a Clerk to a project. Creating Authentication.

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -2,11 +2,22 @@
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 
+// Import Components from Clerk
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+
 const Document = () => {
    return (
       <div>
          <Header>
-            <p className='text-white'>Test</p>
+            <div className='flex w-fit items-center justify-center gap-2'>
+               <p className='document-title'>Share</p>
+            </div>
+            <SignedOut>
+               <SignInButton />
+            </SignedOut>
+            <SignedIn>
+               <UserButton />
+            </SignedIn>
          </Header>
          <Editor />
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
+// Import Fonts
 import { Inter as FontSans } from 'next/font/google'
 
+// Import Css styles & CN
 import './globals.css'
 import { cn } from '@/lib/utils'
+
+// Import Components from Clerk
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
 const fontSans = FontSans({
    subsets: ['latin'],
@@ -19,15 +25,22 @@ export default function RootLayout({
    children: React.ReactNode
 }) {
    return (
-      <html lang='en' suppressHydrationWarning>
-         <body
-            className={cn(
-               'min-h-screen font-sans antialiased',
-               fontSans.variable
-            )}
-         >
-            {children}
-         </body>
-      </html>
+      <ClerkProvider
+         appearance={{
+            baseTheme: dark,
+            variables: { colorPrimary: '#3371FF', fontSize: '16px' },
+         }}
+      >
+         <html lang='en' suppressHydrationWarning>
+            <body
+               className={cn(
+                  'min-h-screen font-sans antialiased',
+                  fontSans.variable
+               )}
+            >
+               {children}
+            </body>
+         </html>
+      </ClerkProvider>
    )
 }
