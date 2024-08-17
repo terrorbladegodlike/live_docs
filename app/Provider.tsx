ad3114fe34da46d586bca7e375ec018071a672be
terrorbladegodlike <ternovetchii.2002@mail.ru>
2024-08-17 22:29:15 +0300

    Collaborative Editor Room

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -12,10 +12,18 @@ import {
 
 // Import Components
 import Loader from '@/components/Loader'
+import { getClerkUsers } from '@/lib/actions/user.actions'
 
 const Provider = ({ children }: { children: ReactNode }) => {
    return (
-      <LiveblocksProvider authEndpoint='/api/liveblocks-auth'>
+      <LiveblocksProvider
+         authEndpoint='/api/liveblocks-auth'
+         resolveUsers={async ({ userIds }) => {
+            const users = await getClerkUsers({ userIds })
+
+            return users
+         }}
+      >
          <ClientSideSuspense fallback={<Loader />}>
             {children}
          </ClientSideSuspense>
