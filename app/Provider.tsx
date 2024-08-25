67d0afefe0e82699b50de357b5003e9f9a4dd026
terrorbladegodlike <ternovetchii.2002@mail.ru>
2024-08-25 17:02:00 +0300

    User Mention Feature

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -12,9 +12,12 @@ import {
 
 // Import Components
 import Loader from '@/components/Loader'
-import { getClerkUsers } from '@/lib/actions/user.actions'
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.actions'
+import { useUser } from '@clerk/nextjs'
 
 const Provider = ({ children }: { children: ReactNode }) => {
+   const { user: clerkUser } = useUser()
+
    return (
       <LiveblocksProvider
          authEndpoint='/api/liveblocks-auth'
@@ -22,6 +25,15 @@ const Provider = ({ children }: { children: ReactNode }) => {
             const users = await getClerkUsers({ userIds })
 
             return users
+         }}
+         resolveMentionSuggestions={async ({ text, roomId }) => {
+            const roomUsers = await getDocumentUsers({
+               roomId,
+               currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+               text,
+            })
+
+            return roomUsers
          }}
       >
          <ClientSideSuspense fallback={<Loader />}>
